2129ecb55925bbc5430e55b0b6adbaf7b73e8dd5
Carl Bradbury <carl@gnarlydog.co.uk>
2020-05-07 18:10:00 +0100

    Photos and text displayed

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,20 +25,15 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          // maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
       </div>
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,49 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import styled from "styled-components"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const url = ".netlify/functions/getInstagramPosts"
+
+const GramContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  a {
+    flex: 1;
+  }
+`
+
+function useInstagram() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        setPosts(data)
+      })
+  }, [])
+  console.log(posts)
+  return posts
+}
+
+const IndexPage = () => {
+  const gramz = useInstagram()
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Netlify/Gatsby Functions Test</h1>
+      <GramContainer>
+        {gramz.map(gram => (
+          <a href={gram.url} key={gram.id}>
+            <img src={gram.thumbnail} alt={gram.caption} />{" "}
+            <p dangerouslySetInnerHTML={{ __html: gram.caption }}></p>
+          </a>
+        ))}
+      </GramContainer>
+    </Layout>
+  )
+}
 
 export default IndexPage
